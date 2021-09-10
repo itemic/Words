43421458e8298f40ef69d5f493d66a13f62a0bea
Terran Kroft <terrankroft@gmail.com>
2021-09-10 17:31:47 +1000

    Initial Commit

---
+++ Words/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Words
+//
+//  Created by Terran Kroft on 9/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Words/WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsApp.swift
+//  Words
+//
+//  Created by Terran Kroft on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
